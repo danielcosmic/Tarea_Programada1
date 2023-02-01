ce1d67c84ee633ab094017259421d59452abe876
Raquel <123610323+rxquelrojxs@users.noreply.github.com>
2023-02-01 00:10:12 -0600

    yuh

--- Tarea1/ListaEstudiantes.java
+++ Tarea1/ListaEstudiantes.java
@@ -1,54 +1,255 @@
+//TAREA PROGRAMADA 1
+//DANIEL ROJAS MORALES C26836
+//RAQUEL ROJAS CASTILLO C26766
+import java.util.HashMap;
+import java.util.Map;
 public class ListaEstudiantes {
 //atributo lista de objetos de la clase Estudiante
-    private Estudiante ListaEstudiantes[];
+    private Estudiante listaEstudiantes[];
+//constructor
+public ListaEstudiantes(Estudiante[] listaEstudiantes) {
+    this.listaEstudiantes = listaEstudiantes;
+}
 //setter y getter
-    public void setListaEstudiantes(Estudiante[] ListaEstudiantes) {
-        this.ListaEstudiantes = ListaEstudiantes;
+    public void setListaEstudiantes(Estudiante[] listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
     }
     public Estudiante[] getListaEstudiantes() {
-        return ListaEstudiantes;
+        return listaEstudiantes;
     }
 //fin setter y getter
 //métodos de la clase ListaEstudiantes
 //método para imprimir la lista de estudiantes
-    public static void imprimirListaEstudiantes(Estudiante[] ListaEstudiantes){
+    public static void imprimirListaEstudiantes(Estudiante[] listaEstudiantes){
         try{
-            for (int i=0; i<ListaEstudiantes.length; i++){
+            for (int i=0; i<listaEstudiantes.length; i++){
                 //impresión sugerida 2
                 System.out.println("Expediente #" + i+1 );
-                System.out.println("Nombre: " + ListaEstudiantes[i].getNombre());
-                System.out.println("Apellidos: " + ListaEstudiantes[i].getApellidos());
-                System.out.println("Cédula: " + ListaEstudiantes[i].getCedula());
-                System.out.println("Carné: " + ListaEstudiantes[i].getCarne());
-                System.out.println("Promedio Ponderado: " + ListaEstudiantes[i].getPromedioPonderado());
-                System.out.println("Carrera: " + ListaEstudiantes[i].getCarrera());
-                System.out.println("Edad: " + ListaEstudiantes[i].getEdad());
+                System.out.println("Nombre: " + listaEstudiantes[i].getNombre());
+                System.out.println("Apellidos: " + listaEstudiantes[i].getApellidos());
+                System.out.println("Cédula: " + listaEstudiantes[i].getCedula());
+                System.out.println("Carné: " + listaEstudiantes[i].getCarne());
+                System.out.println("Promedio Ponderado: " + listaEstudiantes[i].getPromedioPonderado());
+                System.out.println("Carrera: " + listaEstudiantes[i].getCarrera());
+                System.out.println("Edad: " + listaEstudiantes[i].getEdad());
             }
         }
-        catch(Exception ex){//REVISAR ESTA EXCEPCION!!-R
-            throw ex;//REVISAR ESTA EXCEPCION!!-R
-        }
+        catch(Exception e){
+            System.out.println("Error en el arreglo");
+        }//fin try
     }//fin del método imprimirListaEstudiantes
+
 //método para insertar un elemento al inicio de la lista
-    public static void insertarElementoAlInicio(Estudiante ListaEstudiantes[], Estudiante estudianteNuevo){
+    public static void insertarElementoAlInicio(Estudiante listaEstudiantes[], Estudiante estudianteNuevo){
         Estudiante nListaEstudiantes[];
-        nListaEstudiantes = new Estudiante[ListaEstudiantes.length + 1];
+        nListaEstudiantes = new Estudiante[listaEstudiantes.length + 1];
         nListaEstudiantes[0] = estudianteNuevo;
-        for (int i=1; i<ListaEstudiantes.length+1; i++){
-            nListaEstudiantes[i] = ListaEstudiantes[i-1];
+        for (int i=1; i<listaEstudiantes.length+1; i++){
+            nListaEstudiantes[i] = listaEstudiantes[i-1];
         }
-        ListaEstudiantes = nListaEstudiantes;
+        listaEstudiantes = nListaEstudiantes;
+        nListaEstudiantes = null;
     }
+
 //método para insertar un elemento al final de la lista
-    public static void insertarElementoAlFinal(Estudiante ListaEstudiantes[], Estudiante estudianteNuevo){
+    public static void insertarElementoAlFinal(Estudiante listaEstudiantes[], Estudiante estudianteNuevo){
         Estudiante nListaEstudiantes[];
-        nListaEstudiantes = new Estudiante[ListaEstudiantes.length + 1];
-        for (int i=0; i<ListaEstudiantes.length; i++){
-            nListaEstudiantes[i] = ListaEstudiantes[i];
+        nListaEstudiantes = new Estudiante[listaEstudiantes.length + 1];
+        for (int i=0; i<listaEstudiantes.length; i++){
+            nListaEstudiantes[i] = listaEstudiantes[i];
         }
-        nListaEstudiantes[ListaEstudiantes.length] = estudianteNuevo;
-        ListaEstudiantes = nListaEstudiantes;
+        nListaEstudiantes[listaEstudiantes.length] = estudianteNuevo;
+        listaEstudiantes = nListaEstudiantes;
+        nListaEstudiantes = null;
     }// fin del método insertarElementoAlFinal
+
+//método para eliminar un estudiante por cédula
+    public static void eliminarElemento(Estudiante listaEstudiantes[], int estudianteID){
+        Estudiante nListaEstudiantes[];
+        int indice = 0, cuantos = 0;
+        boolean encontrado = false;
+        for(int i=0; i<listaEstudiantes.length; i++){
+            if (estudianteID == listaEstudiantes[i].getCedula()){
+                //se buscan los elementos a borrar dentro del array y se contabilizan
+                encontrado = true;
+                cuantos++;
+            }
+        }// fin for
+
+        if(encontrado = true){
+            //se usa un array auxiliar con un tamaño reducido para almacenar los elementos que se mantienen
+            nListaEstudiantes = new Estudiante[listaEstudiantes.length - cuantos];
+            for(int i=0; i<listaEstudiantes.length; i++){
+                if(estudianteID != listaEstudiantes[i].getCedula()){
+                    //se copian los elementos que se desean mantener y si el elemento a borrar se encuentra en el arreglo el contador del indice no aumenta
+                    nListaEstudiantes[indice] = listaEstudiantes[i];
+                    //el indice solo avanza si se copió un elemento al array
+                    indice++;
+                }
+            }// fin for
+            listaEstudiantes = nListaEstudiantes;
+            nListaEstudiantes = null;
+        }
+    }// fin del método elimninarElemento por cedula
+
+// método para almacenar los elementos a eliminar
+    public static int[] insertarElementoAEliminar(int [] posicionesA, int elemento){
+        int[] nListaEstudiantesA = new int[posicionesA.length + 1];
+        //funcion auxiliar que almacena en un array auxiliar los elementos que se desean eliminar
+        for (int i=0; i<posicionesA.length; i++){
+            nListaEstudiantesA[i] = posicionesA[i];
+        }// fin for
+        nListaEstudiantesA[posicionesA.length] = elemento;
+        posicionesA = nListaEstudiantesA;
+        return posicionesA;
+
+    } // fin del método insertarElementoAEliminar
+
+//método para buscar el indice del elemento a eliminar
+    public static boolean buscarIndice(int [] posicionesA, int elemento){
+        boolean encontrado = false;
+        //funcion auxiliar para buscar los elementos a borrar
+        for(int i=0; i<posicionesA.length; i++){
+            if (elemento == posicionesA[i]){
+                encontrado = true;
+            }
+        }// fin for
+        return encontrado;
+    } // fin del método buscarIndice del elemento a eliminar
+
+//método para eliminar un estudiante por carne
+    public static void eliminarElemento(Estudiante listaEstudiantes[], String estudianteID){
+        Estudiante nListaEstudiantes[];
+        int[] posiciones = new int [0];
+        boolean encontrado = false;
+        //se buscan los elementos a borrar en el array y se envian a una función que almacena sus posiciones
+        for (int i=0; i<listaEstudiantes.length; i++){
+            if(estudianteID.equals(listaEstudiantes[i].getCarne())){
+                encontrado = true;
+                posiciones = insertarElementoAEliminar(posiciones, i);
+            }
+        }// fin for
+        if (encontrado){
+            //si existe un elemento a eliminar dentro del array se borra saltandose su posició cuando se copia la lista a otro array
+            nListaEstudiantes = new Estudiante[listaEstudiantes.length - posiciones.length];
+            int indice = 0;
+            for(int i=0; i < listaEstudiantes.length; i++){
+                if(buscarIndice(posiciones, i) == false){
+                    nListaEstudiantes[indice] = listaEstudiantes[i];
+                    indice ++;
+                }
+            }// fin for
+            listaEstudiantes = nListaEstudiantes;
+            nListaEstudiantes = null;
+        }
+    }// fin del método elimninarElemento por carne
+
+//método para eliminar un elemento por su posición
+    public static void eliminarElementoPorPosicion(Estudiante listaEstudiantes[], int posicion){
+        for (int i = posicion; i < listaEstudiantes.length - 1; i++) {
+            listaEstudiantes[i] = listaEstudiantes[i + 1];
+        }
+        //se mueven todos los elementos despues de la posición indicada un lugar a la izquierda
+        //dejando el elemento a eliminar al final del array e igualandolo a null
+        listaEstudiantes[listaEstudiantes.length - 1] = null;
+
+    }
+
+//método para buscar datos en la lista por cédula
+    public static void buscarPorCedula (Estudiante listaEstudiantes[], int cedula){
+        for (int i=0; i<listaEstudiantes.length; i++){
+            if (cedula == listaEstudiantes[i].getCedula()){
+                System.out.println("Expediente #" + i+1 );
+                System.out.println("Nombre: " + listaEstudiantes[i].getNombre());
+                System.out.println("Apellidos: " + listaEstudiantes[i].getApellidos());
+                System.out.println("Cédula: " + listaEstudiantes[i].getCedula());
+                System.out.println("Carné: " + listaEstudiantes[i].getCarne());
+                System.out.println("Promedio Ponderado: " + listaEstudiantes[i].getPromedioPonderado());
+                System.out.println("Carrera: " + listaEstudiantes[i].getCarrera());
+                System.out.println("Edad: " + listaEstudiantes[i].getEdad());
+            }
+            else{
+                System.out.println("No existe un registro que cumpla con el criterio de búsqueda");
+            }//fin if
+        }//fin for
+    }// fin del método buscarPorCedula
+
+//método para buscar datos en la lista por carne
+    public static void buscarPorCarne (Estudiante listaEstudiantes[], String carne){
+        for (int i=0; i<listaEstudiantes.length; i++){
+            if (carne.equals(listaEstudiantes[i].getCarne())){
+                System.out.println("Expediente #" + i+1 );
+                System.out.println("Nombre: " + listaEstudiantes[i].getNombre());
+                System.out.println("Apellidos: " + listaEstudiantes[i].getApellidos());
+                System.out.println("Cédula: " + listaEstudiantes[i].getCedula());
+                System.out.println("Carné: " + listaEstudiantes[i].getCarne());
+                System.out.println("Promedio Ponderado: " + listaEstudiantes[i].getPromedioPonderado());
+                System.out.println("Carrera: " + listaEstudiantes[i].getCarrera());
+                System.out.println("Edad: " + listaEstudiantes[i].getEdad());
+            }
+            else{
+                System.out.println("No existe un registro que cumpla con el criterio de búsqueda");
+            }//fin if
+        }//fin for
+    }// fin del método buscarPorCarne
+
+//método para obtener información de la lista
+    public static void obtenerInformacion (Estudiante listaEstudiantes[], int opcion){
+        switch (opcion) {
+            case 1:
+            //cantidad de elementos de la lista
+                System.out.println("La lista tiene " + listaEstudiantes.length + " elementos");
+                break;
+            case 2:
+            //Mayor promedio y menor promedio
+                double promedioMayor = listaEstudiantes[0].getPromedioPonderado();
+                double promedioMenor = listaEstudiantes[0].getPromedioPonderado();
+                for (int i=0; i<listaEstudiantes.length; i++){
+                    if(listaEstudiantes[i].getPromedioPonderado() > promedioMayor){
+                        promedioMayor = listaEstudiantes[i].getPromedioPonderado();
+                    }//fin if
+                    if(listaEstudiantes[i].getPromedioPonderado() < promedioMenor){
+                        promedioMenor = listaEstudiantes[i].getPromedioPonderado();
+                    }//fin if
+                }//fin for
+                System.out.println("El mayor promedio ponderado es: " + promedioMayor);
+                System.out.println("El menor promedio ponderado es: " + promedioMenor);
+                break;
+            case 3:
+            //Carrera con más alumnos
+                Map<String, Integer> frecuencias = new HashMap<>();
+                for (int i = 0; i < listaEstudiantes.length; i++) {
+                    String carrera = listaEstudiantes[i].getCarrera();
+                    frecuencias.put(carrera, frecuencias.getOrDefault(carrera, 0) + 1);
+                }
+
+                String carreraMasFrecuente = null;
+                int maxFrecuencia = 0;
+                //utilizando HashMap y Map de la librería util de java se calcula la frecuencia de la carrera contando el número de veces
+                //que se encuentra en el array y se cambia el valor max cuando hay una carrera con mayor frecuencia que la anterior
+                for (Map.Entry<String, Integer> entrada : frecuencias.entrySet()) {
+                    String carrera = entrada.getKey();
+                    int frecuencia = entrada.getValue();
+                    if (frecuencia > maxFrecuencia) {
+                        maxFrecuencia = frecuencia;
+                        carreraMasFrecuente = carrera;
+                    }
+                }// fin for
+                System.out.println("La carrera con más alumnos es: " + carreraMasFrecuente);
+                break;
+            case 4:
+            //Promedio de las edades
+                int sumaEdades = 0;
+                int promedioEdad = 0;
+                    for (int i=0; i<listaEstudiantes.length; i++){
+                        sumaEdades = sumaEdades + listaEstudiantes[i].getEdad();
+                    }//fin for
+                    promedioEdad = sumaEdades / listaEstudiantes.length;
+                    System.out.println("El promedio de las edades es: " + promedioEdad);
+                break;
+            default:
+                System.out.println("Opción inválida");
+                break;
+        }//fin switch
+    }// fin del método obtenerInformacion
 }//fin de la clase
-//COMENTARIOS
-//todos los métodos los he hecho static, si quiere les cambia eso. Todavía no sé para que son jaja. -R
--- Tarea1/Main.java
+++ Tarea1/Main.java
@@ -1,7 +1,11 @@
+//DANIEL ROJAS MORALES C26836
+//RAQUEL ROJAS CASTILLO C26766
 import java.util.*;
 public class Main {
     public static void main (String args[]){
-        Scanner entrada = new Scanner(System.in);
+        Estudiante[] listaEstudiantes;
+        listaEstudiantes = new Estudiante[0];
+        Scanner in = new Scanner(System.in);
         int opcion = 0;
         do {
             System.out.println("Menú");
@@ -14,34 +18,126 @@ public class Main {
             System.out.println("6. Consultar elementos en la lista");
             System.out.println("7. Obtener información de la lista");
             System.out.println("8. Salir");
-            opcion = entrada.nextInt();
+            opcion = in.nextInt();
             switch (opcion) {
                 case 1:
-                    //
-                    break;
+                    //insertar elemento al inicio
+                System.out.println("Ingrese el nombre del estudiante que desea agregar:");
+                String nombrei = in.nextLine();
+                System.out.println("Ingrese los apellidos del estudiante que desea agregar:");
+                String apellidosi = in.nextLine();
+                System.out.println("Ingrese la cédula del estudiante que desea agregar:");
+                String cedulaEi = in.nextLine();
+                int cedulai = Integer.parseInt(cedulaEi);
+                System.out.println("Ingrese el carné del estudiante que desea agregar:");
+                String carnei = in.nextLine();
+                System.out.println("Ingrese el promedio ponderado del estudiante que desea agregar:");
+                String promedioEi = in.nextLine();
+                double promedioPonderadoi = Double.parseDouble(promedioEi);
+                System.out.println("Ingrese la carrera del estudiante que desea agregar:");
+                String carrerai = in.nextLine();
+                System.out.println("Ingrese la cédula del estudiante que desea agregar:");
+                String edadEi = in.nextLine();
+                int edadi = Integer.parseInt(edadEi);
+                Estudiante estudianteNi = new Estudiante(nombrei, apellidosi, cedulai, carnei, promedioPonderadoi, carrerai, edadi);
+                ListaEstudiantes.insertarElementoAlInicio(listaEstudiantes, estudianteNi);
                 case 2:
-                    //
+                    //insertar elemento al final
+                    System.out.println("Ingrese el nombre del estudiante que desea agregar:");
+                String nombref = in.nextLine();
+                System.out.println("Ingrese los apellidos del estudiante que desea agregar:");
+                String apellidosf = in.nextLine();
+                System.out.println("Ingrese la cédula del estudiante que desea agregar:");
+                String cedulaEf = in.nextLine();
+                int cedulaf = Integer.parseInt(cedulaEf);
+                System.out.println("Ingrese el carné del estudiante que desea agregar:");
+                String carnef = in.nextLine();
+                System.out.println("Ingrese el promedio ponderado del estudiante que desea agregar:");
+                String promedioEf = in.nextLine();
+                double promedioPonderadof = Double.parseDouble(promedioEf);
+                System.out.println("Ingrese la carrera del estudiante que desea agregar:");
+                String carreraf = in.nextLine();
+                System.out.println("Ingrese la cédula del estudiante que desea agregar:");
+                String edadEf = in.nextLine();
+                int edadf = Integer.parseInt(edadEf);
+                Estudiante estudianteNf= new Estudiante(nombref, apellidosf, cedulaf, carnef, promedioPonderadof, carreraf, edadf);
+                ListaEstudiantes.insertarElementoAlInicio(listaEstudiantes, estudianteNf);
                     break;
                 case 3:
-                    //
+                    //eliminar por id
+                    System.out.println("Digite 1 para eliminar por cédula");
+                    System.out.println("Digite 2 para eliminar por carné");
+                    String entradaE = in.nextLine();
+                    int opcionE = Integer.parseInt(entradaE);
+                    if (opcionE == 1){
+                        System.out.println("Ingrese la cédula del estudiante que desea eliminar:");
+                        String cedulaI = in.nextLine();
+                        int cedulaE = Integer.parseInt(cedulaI);
+                        ListaEstudiantes.eliminarElemento(listaEstudiantes, cedulaE);
+                    }else if (opcionE == 2){
+                        System.out.println("Ingrese el carné del estudiante que desea eliminar:");
+                        String carneE = in.nextLine();
+                        ListaEstudiantes.eliminarElemento(listaEstudiantes, carneE);
+                    }else{
+                    System.out.println("Opción inválida");
+                    }//fin if
                     break;
                 case 4:
-                    //
+                    //eliminar por posición
+                    System.out.println("Ingrese la posición del estudiante que desea eliminar:");
+                        String posicionIE = in.nextLine();
+                        //se le resta 1 para que quede el indice del elemento a eliminar
+                        int posicionE = Integer.parseInt(posicionIE) - 1;
+                        ListaEstudiantes.eliminarElementoPorPosicion(listaEstudiantes, posicionE);
                     break;
                 case 5:
-                    //
+                    //imprimir lista
+                    ListaEstudiantes.imprimirListaEstudiantes(listaEstudiantes);
                     break;
                 case 6:
-                    //
+                    //consultar elementos
+                    System.out.println("Digite 1 para consultar por cédula");
+                    System.out.println("Digite 2 para consultar por carné");
+                    String entradaC = in.nextLine();
+                    int opcionC = Integer.parseInt(entradaC);
+                    if (opcionC == 1){
+                        System.out.println("Ingrese la cédula del estudiante que desea consultar:");
+                        String cedulaIC = in.nextLine();
+                        int cedulaC = Integer.parseInt(cedulaIC);
+                        ListaEstudiantes.buscarPorCedula(listaEstudiantes, cedulaC);
+                    }else if (opcionC == 2){
+                        System.out.println("Ingrese el carné del estudiante que desea consultar:");
+                        String carneC = in.nextLine();
+                        ListaEstudiantes.buscarPorCarne(listaEstudiantes, carneC);
+                    }else{
+                    System.out.println("Opción inválida");
+                    }//fin if
                     break;
                 case 7:
-                    //
+                    //obtener información
+                    System.out.println("Digite 1 para obtener la cantidad de elementos del arreglo");
+                    System.out.println("Digite 2 para obtener el promedio mayor y menor");
+                    System.out.println("Digite 3 para obtener la carrera con más alumnos");
+                    System.out.println("Digite 4 para obtener el promedio de edades");
+                    String opcionIO = in.nextLine();
+                    int opcionO = Integer.parseInt(opcionIO);
+                    if (opcionO==1){
+                        ListaEstudiantes.obtenerInformacion(listaEstudiantes, opcionO);
+                    }else if (opcionO==2){
+                        ListaEstudiantes.obtenerInformacion(listaEstudiantes, opcionO);
+                    }else if (opcionO==3){
+                        ListaEstudiantes.obtenerInformacion(listaEstudiantes, opcionO);
+                    }else if (opcionO==4){
+                        ListaEstudiantes.obtenerInformacion(listaEstudiantes, opcionO);
+                    }else{
+                        System.out.println("Opción inválida");
+                    }//fin if
                     break;
                 default:
-                    //
+                    System.out.println("Entrada incorrecta");
                     break;
             }//fin switch
         } while (opcion !=8);
-        entrada.close();
+        in.close();
     }//fin del main
 }//fin de la clase
